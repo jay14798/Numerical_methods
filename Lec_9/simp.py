6019db94f49f4eceef2f25e2c0cc7aaaa2ac112b
Jay <jpandey14798@gmail.com>
2022-01-25 02:24:41 +0530

    simp2

--- Lec_9/simp.py
+++ Lec_9/simp.py
@@ -1,12 +1,17 @@
-import numpy as np;
 def f(x):
     return 10 - 3*(x**2);
 
-b = 3;
-a = -1;
-n = np.linspace(a,b,4)
-inf = (b-a)/(3*4);
+b = 3
+a = -1
+n = 4
+inf = (b-a)/n
 Sum = 0;
-for i in range(a,b,4):
-    Sum += inf*(f(n[i])+f(n[i+1]) +4*f((n[i]+n[i+1])/2))*2;
+for i in range(1,n):
+    if i%2 == 1:
+        Sum += 4*f(a + inf*i)
+    else:
+        Sum += 2*f(a + inf*i)
+
+Sum += f(a) + f(b)
+Sum = Sum*(b-a)/(3*n)
 print("the integral is ",Sum)
