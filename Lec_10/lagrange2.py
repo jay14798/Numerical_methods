7a85222bf57b04a5b2075a79b2d0497bfd27cb1e
Jay <jpandey14798@gmail.com>
2022-01-25 02:02:27 +0530

    Lag 2

--- Lec_10/lagrange2.py
+++ Lec_10/lagrange2.py
@@ -1,17 +1,17 @@
 x_in = [0,1,2]
 y_in = [1,2,4]
 
-x= 1
+x= 1.5
 y= 0
 
 for i in range(len(x_in)):
     l=1
-    for j in range(len(x_in):
+    for j in range(len(x_in)):
             if(i!=j):
-            l =  l*((x-x_in[j])/(x_in[i]-x_in[j]))
+                l =  l*((x-x_in[j])/(x_in[i]-x_in[j]))
     y += l*y_in[i]
 
-print("the value calculated by interpolation is ",y))
+print("the value calculated by interpolation is ",y)
 
 def g(x):
     return 2**x
